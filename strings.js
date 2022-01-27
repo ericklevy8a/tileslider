3f9358124966b56e6b82f0353f14f67cb393001a
Erick Levy <ericklevy@gmail.com>
2022-01-26 18:10:58 -0600

    Replace strings with strings-en

--- strings.js
+++ strings.js
@@ -1,38 +1,38 @@
 const strings = {
     appTitle: 'Switcher',
-    appLongName: 'Juego de Interruptores',
-    btnStart: 'Iniciar',
-    btnHelp: 'Ayuda',
-    btnAbout: 'Acerca',
-    btnStats: 'Estadísticas',
-    btnConfig: 'Configuración',
-    btnOk: 'Aceptar',
-    btnCancel: 'Cancelar',
-    configboxTitle: 'Configuración',
-    configboxPlaySoundsLabel: 'Reproducir sonidos:',
-    configboxSwitchStyleLabel: 'Estilo de interruptor:',
-    configboxColorSchemeLabel: 'Esquema de color:',
-    statsboxTitle: 'Estadísticas',
-    statsboxGamesLabel: 'Juegos:',
-    gamesPlayed: 'Jugados',
-    gamesWinned: 'Ganados',
-    statsboxGraphLabel: 'Gráfico de Distribución de Pasos:',
-    helpTitle: 'Cómo Jugar',
-    helpMessage: `El objetivo de este juego es <b>ENCENDER</b> todos los interruptores.<br><br>
-        Cambiando un interruptor tambien cambia a los interruptores adyacentes
-        (arriba, abajo y latrales, no diagonles).<br><br>
-        <b>Intenta hacerlo sin pasarte de nueve pasos!</b>`,
-    aboutTitle: 'Acerca del Juego',
-    aboutMessage: `© Copyright 2019 por Erick Levy<br><br>
-        Este es un refrito de un viejo juego que codifiqué por primera vez en Java para Android,
-        y ahora lo he portado a <b>HTML5, CSS3 y Javacript</b> para mi area de pruebas de habilidades Web.<br><br>
-        Gracias por tomarte el tiempo para conocerlo y jugar con esta pequeña app.`,
-    restartTitle: 'Juego Inconcluso',
-    restartMessage: 'Estás seguro de reiniciar el juego?',
-    lblSteps: '? pasos...',
-    gameOverTitle: 'Fin del Juego',
-    gameOverScore: 'Lo has logrado en ? pasos...<br><br>',
-    gameOverWin: 'Felicitaciones!',
-    gameOverTry: 'Puedes hacerlo mejor!',
-    pressStart: 'Presiona el botón para INICIAR...'
+    appLongName: 'The Switcher Game',
+    btnStart: 'Start',
+    btnHelp: 'Help',
+    btnAbout: 'About',
+    btnStats: 'Statistics',
+    btnConfig: 'Configuration',
+    btnOk: 'Ok',
+    btnCancel: 'Cancel',
+    configboxTitle: 'Configuration',
+    configboxPlaySoundsLabel: 'Play sounds:',
+    configboxSwitchStyleLabel: 'Switch style:',
+    configboxColorSchemeLabel: 'Color scheme:',
+    statsboxTitle: 'Statistics',
+    statsboxGamesLabel: 'Games:',
+    gamesPlayed: 'Played',
+    gamesWinned: 'Winned',
+    statsboxGraphLabel: 'Steps Distribution Graph:',
+    helpTitle: 'How to Play',
+    helpMessage: `The goal of this game is to <b>TURN ON</b> all the nine switches.<br><br>
+        Changing a switch also changes the adjacent switches
+        (top, bottom and sides, not diagonally).<br><br>
+        <b>Try to do it without going over nine steps!</b>`,
+    aboutTitle: 'About the Game',
+    aboutMessage: `© Copyright 2019 by Erick Levy<br><br>
+        This is a remake of an old simple game coded first in Java for Android,
+        and ported to <b>HTML5, CSS3 and Javacript</b> for my web skills test area.<br><br>
+        Thank you for taking the time to learn about and play with this little app.`,
+    restartTitle: 'Game Incomplete',
+    restartMessage: 'Are you sure to restart the game?',
+    lblSteps: '? steps...',
+    gameOverTitle: 'Game Over',
+    gameOverScore: 'You have achieved it in ? steps...<br><br>',
+    gameOverWin: 'Congratulations!',
+    gameOverTry: 'You can do it better!',
+    pressStart: 'Press the START button...'
 }
